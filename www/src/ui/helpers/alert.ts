bb3fd0191809fc6f65d6a54e4175b4451c7df8b6
Rlzee <romainprovencel@icloud.com>
2025-06-24 16:29:42 +0200

    feat: implement alert function to manage AlertDialog interactions

---
+++ www/src/ui/helpers/alert.ts
@@ -0,0 +1,14 @@
+import { AlertDialogParams, AlertDialogPortalName } from "@/src/ui/components/alert-dialog";
+import { usePortal } from "@/src/ui/stores/portal.store";
+
+export function alert(params: AlertDialogParams) {
+  const state = usePortal.getState();
+
+  if (!state) {
+    console.warn("Portal store not initialized.");
+    return;
+  }
+
+  state.openPortal(AlertDialogPortalName, params);
+}
+
