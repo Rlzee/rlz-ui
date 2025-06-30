55b2e4a7878ef2c3caf9c641009271d1d7d27c67
Rlzee <romainprovencel@icloud.com>
2025-06-30 15:43:00 +0200

    feat: add NavigationMenu component to Navbar for enhanced navigation experience

--- www/src/ui/components/navbar.tsx
+++ www/src/ui/components/navbar.tsx
@@ -14,6 +14,9 @@ import { Menu, X } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/src/ui/components/button";
 import { cn } from "@/src/lib/utils";
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
+import { ChevronDownIcon } from "lucide-react";
+import { cva } from "class-variance-authority";
 
 /* --------------------------- Context & Provider --------------------------- */
 
@@ -126,6 +129,77 @@ const NavbarItem = (props: NavbarItemProps) => {
   );
 };
 
+/* ----------------------------- Navbar Menu Item ------------------------------ */
+
+const NavbarMenuItem = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof NavigationMenuPrimitive.Item>) => {
+  return (
+    <NavigationMenuPrimitive.Root
+      data-slot="navbar-menu"
+      data-viewport={false}
+      className="group/navigation-menu relative flex max-w-max flex-1 items-center justify-center"
+    >
+      <NavigationMenuPrimitive.List
+        data-slot="navbar-menu-list"
+        className="group flex flex-1 list-none items-center justify-center gap-1"
+      >
+        <NavigationMenuPrimitive.Item
+          data-slot="navbar-menu-item"
+          className={cn("relative", className)}
+          {...props}
+        />
+      </NavigationMenuPrimitive.List>
+    </NavigationMenuPrimitive.Root>
+  );
+};
+
+/* ----------------------------- Navbar Menu Trigger ------------------------------ */
+
+const navbarMenuTriggerStyle = cva(
+  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium hover:bg-secondary focus:bg-secondary disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-secondary data-[state=open]:focus:bg-secondary data-[state=open]:bg-secondary/80 outline-none transition-[color,box-shadow] focus-visible:outline-1"
+);
+
+const NavbarMenuTrigger = ({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof NavigationMenuPrimitive.Trigger>) => {
+  return (
+    <NavigationMenuPrimitive.Trigger
+      data-slot="navigation-menu-trigger"
+      className={cn(navbarMenuTriggerStyle(), "group", className)}
+      {...props}
+    >
+      {children}{" "}
+      <ChevronDownIcon
+        className="relative top-[1px] ml-1 size-3 transition duration-300 group-data-[state=open]:rotate-180"
+        aria-hidden="true"
+      />
+    </NavigationMenuPrimitive.Trigger>
+  );
+};
+
+/* ----------------------------- Navbar Menu Content ------------------------------ */
+
+const NavbarMenuContent = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof NavigationMenuPrimitive.Content>) => {
+  return (
+    <NavigationMenuPrimitive.Content
+      data-slot="navigation-menu-content"
+      className={cn(
+        "data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 top-0 left-0 w-full p-2 pr-2.5 md:absolute md:w-auto",
+        "group-data-[viewport=false]/navigation-menu:bg-popover group-data-[viewport=false]/navigation-menu:text-popover-foreground group-data-[viewport=false]/navigation-menu:data-[state=open]:animate-in group-data-[viewport=false]/navigation-menu:data-[state=closed]:animate-out group-data-[viewport=false]/navigation-menu:data-[state=closed]:zoom-out-95 group-data-[viewport=false]/navigation-menu:data-[state=open]:zoom-in-95 group-data-[viewport=false]/navigation-menu:data-[state=open]:fade-in-0 group-data-[viewport=false]/navigation-menu:data-[state=closed]:fade-out-0 group-data-[viewport=false]/navigation-menu:top-full group-data-[viewport=false]/navigation-menu:mt-1.5 group-data-[viewport=false]/navigation-menu:overflow-hidden group-data-[viewport=false]/navigation-menu:rounded-md group-data-[viewport=false]/navigation-menu:border group-data-[viewport=false]/navigation-menu:shadow group-data-[viewport=false]/navigation-menu:duration-200 **:data-[slot=navigation-menu-link]:focus:ring-0 **:data-[slot=navigation-menu-link]:focus:outline-none",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
 /* -------------------------- Responsive Sections -------------------------- */
 
 const NavbarToggle = ({ className }: ComponentProps<typeof Button>) => {
@@ -195,4 +269,7 @@ export {
   NavbarToggle,
   NavbarProvider,
   useNavbar,
+  NavbarMenuItem,
+  NavbarMenuTrigger,
+  NavbarMenuContent,
 };
