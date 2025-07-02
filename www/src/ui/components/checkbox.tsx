4df962dfc9907faa8b4e8f5732c3bc61b1a10933
Rlzee <romainprovencel@icloud.com>
2025-07-03 01:07:31 +0200

    refactor: update Checkbox and Switch components for improved styling and state handling

--- www/src/ui/components/checkbox.tsx
+++ www/src/ui/components/checkbox.tsx
@@ -13,9 +13,10 @@ const Checkbox = ({
   return (
     <CheckboxPrimitive.Root
       data-slot="checkbox"
-      className={cn(
-        "peer size-4 shrink-0 rounded-[4px] border border-border shadow-xs outline-none transition-shadow focus-visible:ring-[3px] focus-visible:border-ring focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50",
-        "bg-secondary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary",
+       className={cn(
+        "data-[state=checked]:from-primary/85 data-[state=checked]:to-primary data-[state=checked]:text-primary-foreground data-[state=checked]:inset-shadow-2xs data-[state=checked]:inset-shadow-white/25 data-[state=checked]:bg-linear-to-b data-[state=checked]:dark:from-primary/75 data-[state=checked]:dark:bg-linear-to-t border data-[state=checked]:border-zinc-50/50 data-[state=checked]:shadow-md data-[state=checked]:shadow-zinc-950/20 ring-0 transition-[filter] duration-200 data-[state=checked]:dark:border-0 data-[state=checked]:dark:border-zinc-950/50",
+        "bg-secondary border border-border",
+        "peer size-4 shrink-0 rounded-[4px] outline-none transition-shadow focus-visible:ring-[3px] focus-visible:border-ring focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50",
         "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
         className
       )}
--- www/src/ui/components/switch.tsx
+++ www/src/ui/components/switch.tsx
@@ -13,7 +13,8 @@ const Switch = ({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[state=checked]:bg-primary focus-visible:border-ring focus-visible:ring-ring/50 data-[state=unchecked]:bg-secondary inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "data-[state=checked]:from-primary/85 data-[state=checked]:to-primary data-[state=checked]:text-primary-foreground data-[state=checked]:inset-shadow-2xs data-[state=checked]:inset-shadow-white/25 data-[state=checked]:bg-linear-to-b data-[state=checked]:dark:from-primary/75 data-[state=checked]:dark:bg-linear-to-t border data-[state=checked]:border-zinc-50/50 data-[state=checked]:shadow-md data-[state=checked]:shadow-zinc-950/20 ring-0 transition-[filter] duration-200 data-[state=checked]:dark:border-0 data-[state=checked]:dark:border-zinc-950/50",
+        "peer focus-visible:border-ring focus-visible:ring-ring/50 data-[state=unchecked]:bg-secondary inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
