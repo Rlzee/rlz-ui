9ed819f734d3f6f866b0ef54da1c78ef51a724c0
Rlzee <romainprovencel@icloud.com>
2025-07-12 00:34:42 +0200

    .

--- www/src/ui/components/navigation-menu.tsx
+++ www/src/ui/components/navigation-menu.tsx
@@ -186,7 +186,7 @@ const NavigationMenuListItem = ({
 }: ComponentPropsWithoutRef<"li"> & { href: string }) => {
   return (
     <li {...props}>
-      <NavigationMenuLink asChild data-slot="navigation-menu-list-item-link">
+      <NavigationMenuLink asChild data-slot="navigation-menu-list-item">
         <Link href={href}>
           <div className="text-sm leading-none font-medium">{title}</div>
           <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
