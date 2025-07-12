b2fbea25cc13f8145d6683539864fd7e373ade29
Rlzee <romainprovencel@icloud.com>
2025-07-12 17:44:17 +0200

    feat: add InputLabelAnimate component for enhanced label behavior and styling

--- www/src/ui/components/custom/input/input-label.tsx
+++ www/src/ui/components/custom/input/input-label.tsx
@@ -63,4 +63,22 @@ const InputInsetLabel = ({
   );
 };
 
-export { InputLabel, InputInsetLabel };
+const InputLabelAnimate = ({ labelText, className, ...props}: InputLabelProps) =>{
+  const id = useId()
+
+  return (
+    <div className="group relative">
+      <label
+        htmlFor={id}
+        className="origin-start text-muted-foreground group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:text-foreground absolute top-1/2 block -translate-y-1/2 cursor-text px-1 text-sm transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-0 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium"
+      >
+        <span className="bg-background inline-flex px-2">
+            {labelText}
+        </span>
+      </label>
+      <Input id={id} className={cn("bg-background", className)} {...props} />
+    </div>
+  )
+}
+
+export { InputLabel, InputInsetLabel, InputLabelAnimate };
