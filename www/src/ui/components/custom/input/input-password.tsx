23538319609eb920e43588d76b2c0d6c0ac48068
Rlzee <romainprovencel@icloud.com>
2025-07-29 03:54:07 +0200

    feat: add reverse prop to InputPassword component for customizable button placement

--- www/src/ui/components/custom/input/input-password.tsx
+++ www/src/ui/components/custom/input/input-password.tsx
@@ -7,8 +7,9 @@ import { ComponentProps, useState } from "react";
 
 const InputPassword = ({
   className,
+  reverse = false,
   ...props
-}: ComponentProps<typeof InputAddon>) => {
+}: ComponentProps<typeof InputAddon> & { reverse?: boolean }) => {
   const [visible, setVisible] = useState(false);
 
   return (
@@ -24,20 +25,37 @@ const InputPassword = ({
       className={cn("pr-10", className)}
       {...props}
     >
-      <InputAddon.Right>
-        <button
-          type="button"
-          onClick={() => setVisible((v) => !v)}
-          aria-label={visible ? "Hide the password" : "Show password"}
-          tabIndex={-1}
-        >
-          {visible ? (
-            <EyeOff className="h-4 w-4" />
-          ) : (
-            <Eye className="h-4 w-4" />
-          )}
-        </button>
-      </InputAddon.Right>
+      {reverse ? (
+        <InputAddon.Right>
+          <button
+            type="button"
+            onClick={() => setVisible((v) => !v)}
+            aria-label={visible ? "Hide the password" : "Show password"}
+            tabIndex={-1}
+          >
+            {visible ? (
+              <EyeOff className="h-4 w-4" />
+            ) : (
+              <Eye className="h-4 w-4" />
+            )}
+          </button>
+        </InputAddon.Right>
+      ) : (
+        <InputAddon.Left>
+          <button
+            type="button"
+            onClick={() => setVisible((v) => !v)}
+            aria-label={visible ? "Hide the password" : "Show password"}
+            tabIndex={-1}
+          >
+            {visible ? (
+              <EyeOff className="h-4 w-4" />
+            ) : (
+              <Eye className="h-4 w-4" />
+            )}
+          </button>
+        </InputAddon.Left>
+      )}
     </InputAddon>
   );
 };
