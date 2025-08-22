be68c27dbca7a7aee64069cd9fe9f0fa4946314c
Rlzee <romainprovencel@icloud.com>
2025-08-22 19:41:08 +0200

    feat: update DocPage to utilize contentlayer for document retrieval, enhance index.mdx with links, and configure tsconfig for base URL

--- apps/www/app/(app)/docs/[[...slug]]/page.tsx
+++ apps/www/app/(app)/docs/[[...slug]]/page.tsx
@@ -1,7 +1,53 @@
-export default function DocPage() {
+import { allDocs, type Doc } from ".contentlayer/generated";
+import { notFound } from "next/navigation";
+
+import { ArrowUpRight } from "lucide-react";
+
+interface DocPageProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function getDocFromParams({ params }: DocPageProps) {
+  const slug = params.slug?.join("/") || "";
+  const doc = allDocs.find((doc: Doc) => doc.slugAsParams === slug);
+
+  if (!doc) {
+    return null;
+  }
+
+  return doc;
+}
+
+export default async function DocPage({ params }: DocPageProps) {
+  const doc = await getDocFromParams({ params });
+
+  if (!doc) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Document Page</h1>
+    <div className="flex flex-col flex-1">
+      <div className="flex items-center justify-between mb-8">
+        <p className="text-muted-foreground">{doc.title}</p>
+        {doc.links && (
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-1 cursor-default hover:underline">
+              <a href={doc.links.doc} target="_blank" rel="noreferrer">
+                Docs
+              </a>
+              <ArrowUpRight className="w-4 h-4" />
+            </div>
+            <div className="flex items-center gap-1 cursor-default hover:underline">
+              <a href={doc.links.api} target="_blank" rel="noreferrer">
+                Api
+              </a>
+              <ArrowUpRight className="w-4 h-4" />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
